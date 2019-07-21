892de092dfa3bcad8d93c9eced47577769def804
shashank151 <sanutg@gmail.com>
2019-07-21 08:39:20 +0530

    review_updates

--- sw.js
+++ sw.js
@@ -46,7 +46,9 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
-		caches.match(event.request)
+		caches.match(event.request,{
+			ignoreSearch: true
+		  })
 		.then(function(response) {
 			return response || fetch(event.request);
 		})
